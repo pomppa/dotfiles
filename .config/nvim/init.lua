7c40130e3e0c28dc2fdcfdf221a2f67c7f574579
pomppa <mail@pomppa.xyz>
2024-04-23 17:07:54 +0300

    add fzf.vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -763,6 +763,7 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
   { 'junegunn/fzf' },
+  { 'junegunn/fzf.vim' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
