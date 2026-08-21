1d6e2e882a8244869c829c8caca7662704238370
Tuomas Änäkkälä <tuomas@anakkala.io>
2026-08-21 08:07:53 +0300

    update nv cursor style

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -22,6 +22,22 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
+
+local function apply_cursor_style()
+  vim.opt.guicursor = 'n-v-c:block-Cursor/lCursor,i-ci-ve:ver35-iCursor,r-cr:hor25,o:hor50,a:blinkon0'
+
+  -- Keep cursor and cursorline visible on darker themes.
+  vim.api.nvim_set_hl(0, 'Cursor', { fg = '#1a1b26', bg = '#ff9e64' })
+  vim.api.nvim_set_hl(0, 'lCursor', { fg = '#1a1b26', bg = '#ff9e64' })
+  vim.api.nvim_set_hl(0, 'iCursor', { fg = '#1a1b26', bg = '#7aa2f7' })
+  vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#2a2f4a' })
+end
+
+apply_cursor_style()
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = apply_cursor_style,
+})
+
 vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.opt.termguicolors = true
